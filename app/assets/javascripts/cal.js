df59168adf782d622dd9051502c3c2effa9f09ed
미연 <mi-yeon@miyeon-ui-MacBook-Pro.local>
2017-02-25 05:48:00 +0900

    달력 스타일 수정

--- app/assets/javascripts/cal.js
+++ app/assets/javascripts/cal.js
@@ -4,16 +4,19 @@ init_cal=function(){
       var calendar=$(this);
       calendar.fullCalendar({
         header: {
-                left: false,
-                center: 'title',
-                right: 'agendaWeek'
+                left: 'title',
+                center: false,
+                right: false
         },
-        height: 604,
+        height: 673,
         defaultView: 'agendaWeek',
         selectable: {
             agenda: true
         },
-        selectHelper: true,
+        agendaWeek: {
+          slotLabelFormat: 'hA'
+        },
+        selectHelper: false,
         editable: false,
         allDaySlot: false,
         events: '/reservations.json',
@@ -26,7 +29,12 @@ init_cal=function(){
         monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
         dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'],
         dayNamesShort: ['일','월','화','수','목','금','토'],
-        titleFormat: 'MMMM D'
+        views: {
+          week: {
+              columnFormat: 'D/ddd'
+          }
+        },
+        titleFormat: 'M/Dddd'
     });
   });
 };
